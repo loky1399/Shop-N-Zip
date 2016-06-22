f18fcc64542a42c3a464e9470e7286b99865b099
Lokesh <lokeshaakasapu@gmail.com>
2016-06-22 17:36:05 +0530

    commit 22/06/16

---
+++ src/runner/FAQs_Runner.java
@@ -0,0 +1,13 @@
+package runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "D:\\Shop-N-Zip\\Features\\FAQ.feature", glue = { "stepDefinitions" }, dryRun = false, plugin = {
+		"pretty", "html:Reports/HTML/SignIn.feature",
+		"json:Reports/JsonReports/SingIn.feature.json" },tags={"@MyTest"})
+public class FAQs_Runner {
+
+}
---
+++ src/runner/Registration_Runner.java
@@ -0,0 +1,13 @@
+package runner;
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "D:\\Shop-N-Zip\\Features\\SignUp.feature", glue = { "stepDefinitions" }, dryRun = true, plugin = {
+		"pretty", "html:Reports/HTML/SignIn.feature",
+		"json:Reports/JsonReports/SingIn.feature.json" },tags={"@myTest"})
+
+public class Registration_Runner {
+
+}
